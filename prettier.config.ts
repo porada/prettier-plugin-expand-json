08bf5694731720c98c27ff1ae5497ce1766d73e2
Dom Porada <dom@dom.engineering>
2026-01-23 16:10:41 +0100

    Update dependencies

--- prettier.config.ts
+++ prettier.config.ts
@@ -1,14 +1,8 @@
 import { defineConfig } from '@standard-config/prettier';
 import * as pluginExpandJSON from './src/index.ts';
 
-const config = defineConfig({
-	plugins: [pluginExpandJSON],
+export default defineConfig({
+	pluginOverrides: {
+		'prettier-plugin-expand-json': pluginExpandJSON,
+	},
 });
-
-for (const override of config.overrides ?? []) {
-	if (override.files?.includes('*.json')) {
-		override.options?.plugins?.push(pluginExpandJSON);
-	}
-}
-
-export default config;
